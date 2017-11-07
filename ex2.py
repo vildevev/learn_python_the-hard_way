7208d3cdbe4842ed962fe997211512a783960f3c
“Vilde <vilde@vevatne.no>
2017-11-07 11:12:02 -0800

    Finish exercise 2 and 3

---
+++ ex2.py
@@ -0,0 +1,5 @@
+print("I could have code like this.") # and the comment after is ignored 
+print("This will run.")
+
+# find mistakes by reading your code backwards 
+# or try reading what you've typed out loud
---
+++ ex3.py
@@ -0,0 +1,47 @@
+# Informs the user that chickens will be counted
+print("I will now count my chickens:")
+
+# Prints the number of hens 
+print("Hens", 25 + 30 / 6)
+# Prints the number of roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Informs the user that eggs will be counted
+print("Now I will count the eggs:")
+
+# Calculates and prints out the egg count
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Asks if it's true that the numbers are smaller
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Calculates whether it's True or False
+print(3 + 2 < 5 - 7)
+
+# Prints a question, calculates the answer
+print("What is 3 + 2?", 3 + 2)
+# Prints question and answer
+print("What is 5 - 7?", 5 - 7)
+
+# Informs user that it's false
+print("Oh, that's why it's False.")
+
+# Tells user more calculations will come
+print("How about some more.")
+
+# Asks questions, calculates answers
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+
+# Exercise: 
+
+# Calculate my mood 
+print("Am I happy today?")
+print("Happiness is determined on a scale from 0-100")
+
+print("Is Vilde Happy?", 100-50/3+7%2-40)
+
+print("Oh boy, that's too bad :(")
+print("She should learn to be more optimistic and all that shit")
