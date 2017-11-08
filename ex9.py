27b1e7cf883a6f3b0bdd794ca50d0b8146cc1e11
“Vilde <vilde@vevatne.no>
2017-11-08 13:39:11 -0800

    Finish exercise 9 and 10

---
+++ ex9.py
@@ -0,0 +1,16 @@
+# assign a string on different days
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# String with names of months with line-breaks between them
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# printing out the variables
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# Three quotes puts everything inside as part of the comment
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
