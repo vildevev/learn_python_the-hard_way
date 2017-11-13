2572f355c71090abfa5065e8955251eccd71b864
“Vilde <vilde@vevatne.no>
2017-11-14 00:22:15 +0100

    Fix ex17 one liner

--- ex17.py
+++ ex17.py
@@ -6,7 +6,7 @@ script, from_file, to_file = argv
 # print(f"Copying from {from_file} to {to_file}")
 
 # failed attempt to make it into a one-liner
-open(to_file, 'w').write(open(from_file)).close()
+indata = open(from_file).read()
 # indata = in_file.read()
 
 # print(f"The input file is {len(indata)} bytes long")
@@ -15,10 +15,10 @@ open(to_file, 'w').write(open(from_file)).close()
 # print("Ready, hit RETURN to continue, CRL-C to abort.")
 # input()
 
-# out_file = open(to_file, 'w')
-# out_file.write(indata)
+out_file = open(to_file, 'w')
+out_file.write(indata)
 
 # print("Alright, all done.")
 
-# out_file.close()
+out_file.close()
 # in_file.close()
