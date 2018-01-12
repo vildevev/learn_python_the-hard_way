37b7b63dbd618379c583a8d7399f691d84fd0741
Vilde Vevatne <vvevatne@pinterest.com>
2018-01-11 19:37:39 -0800

    Half of ex 41

---
+++ ex41.py
@@ -0,0 +1,60 @@
+import random 
+from urllib.request import urlopen 
+import sys
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORD = []
+
+PHRASES = {
+	"class %%%(%%%):":
+		"Make a class named %%% that is-a %%%.",
+	"class %%%(object):\n\tdef __init__(self, ***)" :
+		"class %%% has-a __init__ that takes self and *** params.",
+	"class %%%(object):\n\tdef ***(self, @@@)":
+		"class %%% has-a function *** that takes self and @@@ params.",
+	"*** = %%%()":
+		"Set *** to an instance of class %%%.",
+	"***.****(@@@)":
+		"From *** get the *** function, call it with params self, @@@.",
+	"***.*** = '***'":
+		"From *** get the *** attribute and set it to '***'."
+}
+
+# do they want to drill phrases first 
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+	PHRASE_FIRST = True 
+else:
+	PHRASE_FIRST = False 
+
+# load up the words from the website 
+for word in urlopen(WORD_URL).readlines():
+	WORDS.append(str(word.strip(), encoding="utf-8"))
+
+
+
+def convert(snippet, phrase):
+	class_names = [w.capitalize() for w in 
+				    random.sample(WORDS, snippet.count("%%%"))]
+	other_names = random.sample(WORDS, snippet.count("***"))
+	results = []
+	param_names = []
+
+	for i in range(0, snippet.count("@@@")):
+		param_count = random.randint(1,3)
+		param_names.append(', '.join(
+			random.sample(WORDS, param_count)))
+
+	for sentence in snippet, phrase:
+		# this is how you duplicate a list or string 
+		result = sentence[:]
+
+
+		# fake class names
+		for word in class_names:
+			result = result.replace("%%%", word, 1)
+
+		# fake other names 
+		for word in other_names:
+			result = result.replace("***", word, 1)
+
+		# faj
